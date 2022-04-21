f00f794283bc06fe238332e2a4fe1655df1609ef
Efim <42698670+Efim27@users.noreply.github.com>
2022-04-21 14:31:48 +0300

    user list page

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('user/list', views.user_list, name='user_list'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET, require_POST
+from django.contrib.auth.models import User
 
+@require_GET
 def index(request):
-	return render(request, 'main/pages/index.html')
+	if request.user.is_authenticated:
+		return redirect('/user/list')
+
+	return redirect('/signin')
+
+@require_GET
+@login_required
+def user_list(request):
+	user_list = User.objects.all().exclude(id=request.user.id)
+
+	return render(request, 'main/pages/user/list.html', {'userList': user_list})
