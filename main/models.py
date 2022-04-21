957365eb36bdd2c8e2cea4d25f014f8a325623ed
Efim <42698670+Efim27@users.noreply.github.com>
2022-04-21 15:25:24 +0300

    Модель сообщения юзеров

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserMsg(models.Model):
+	id = models.AutoField(primary_key=True)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_messages')
+	recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+	msg_text = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
